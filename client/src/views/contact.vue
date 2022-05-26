3093041d924d13655c1751f085e37dfdbb5a5884
utibe inyangetuk <inyangutibz@gmail.com>
2022-05-26 11:00:13 +0100

    contact form

--- client/src/views/contact.vue
+++ client/src/views/contact.vue
@@ -1,20 +1,70 @@
 <template>
-	<div>
-		<h3>Our developers are still working on this page.</h3>
+	<Navbar/>
+
+	<div class="contact-box">
+		<form>
+		<h2 class="heading">contact us</h2>
+			<input type="text" class="input-field" placeholder="Your email" />
+			<input type="text" class="input-field" placeholder="Message heading" />
+			<textarea
+				class="input-field textarea"
+				type="text"
+				placeholder="Your message"
+				cols="30"
+				rows="30"
+			/>
+			<button type="submit">send message</button>
+		</form>
 	</div>
 </template>
 
 <script>
-	export default {
+	import Navbar from '../components/navbar'
+export default {
+  components: { Navbar },
 		name: "contactUs",
 	};
 </script>
 
 <style scoped>
-	h3 {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		font-size: 20px;
+	.contact-box {
+		background-color: #fff;
+		width: 500px;
+		box-shadow: 0 0 5px 0 #999;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+		border-radius: 5px;
+	}
+	form {
+		margin: 35px;
+	}
+	.heading{
+		text-decoration: underline;
+	}
+	.input-field {
+		border-radius: 5px;
+		width: 400px;
+		height: 40px;
+		margin-top: 15px;
+		padding-left: 10px;
+		border: 1px solid #777;
+		outline: none;
+	}
+
+	.textarea {
+		height: 150px;
+		padding-top: 10px;
+	}
+	button {
+		padding: 8px;
+		margin-top: 10px;
+		border-radius: 5px;
+		letter-spacing: 1px;
+		font-weight: bold;
+		border: 1px solid gray;
+		cursor: pointer;
+		font-size: 15px;
 	}
 </style>
